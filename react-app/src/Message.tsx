376ad2cfbd960ecc31d8391808f1b44652d048a3
TeddyS <szuta.tadeusz@gmail.com>
2024-11-02 13:15:36 +0100

    removed vite app

--- react-app/src/Message.tsx
+++
@@ -1,9 +0,0 @@
-// PascalCasing
-function Message() {
-  // JSX - JavaScript XML
-  const name = "Tadeusz";
-  if (name) return <h1>Hello {name}!</h1>;
-  return <h1>Hello world!</h1>;
-}
-
-export default Message;
